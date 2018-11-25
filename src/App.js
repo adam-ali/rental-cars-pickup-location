d61d2143a6dcf14673bff46b168b62a448ad7628
Adam <adam.a@spartadigital.co.uk>
2018-11-25 23:48:30 +0000

    change input when u select result

--- src/App.js
+++ src/App.js
@@ -7,14 +7,20 @@ class App extends Component {
     
     this.state = {
       searchResults : [],
-      isLoading : false
-        
+      isLoading : false,
+      inputValue: '',
+      showSearch: true,
+      lostFocus: false
     }
-    this.search = this.search.bind(this);    
+    this.search = this.search.bind(this);     
+    this.onFocus = this.onFocus.bind(this);     
    }
+
+
 
    search(e) {
     let inputTxt = e.target.value;
+    this.setState({inputValue: inputTxt})
     // only if search input has more than 1 character search for results
     if (inputTxt.length > 1) {
         this.getSearchResults(inputTxt);
@@ -27,16 +33,17 @@ class App extends Component {
   }
 
   getSearchResults(searchTerm){
-    // number of results to be returned 
-    let NoOfResults = 6;
-    this.setState({isLoading: true});
+    let NoOfResults = 6;// number of results to be returned
+    this.setState({isLoading: true}); //show loading indicator
     fetch(`https://cors.io/?https://www.rentalcars.com/FTSAutocomplete.do?solrIndex=fts_en&solrRows=${NoOfResults}&solrTerm=${searchTerm}`)
     .then(res=> res.json())
     .then(res=>{
-      this.setState({isLoading: false});
-    
+      this.setState({isLoading: false}); // hide loading indicator
+
       let results = res.results.docs;  
-      console.log(results);
+
+      /* because the request is asynchronous another check is needed to see if the input values
+       have changed since the results from the request was recieved */
       if (this.searchInput.value.length<2) {
         this.setState({
           searchResults: []
@@ -48,15 +55,23 @@ class App extends Component {
       }
     })
   }
+  onFocus(){
+    this.setState({lostFocus: false})    
+  }
+  selectLocation(name){
+    this.setState({
+      inputValue:name,
+      lostFocus:true
+    });
+    
+  }
 
   render() {
     let resultsFound = true;      
-
     if (this.state.searchResults[0] && this.state.searchResults[0].name === 'No results found'){
         resultsFound = false;
     }
     else resultsFound = true;
-    console.log(this.state)
     return (
       <div className="App">
 
@@ -64,29 +79,34 @@ class App extends Component {
           <h2>Let’s find your ideal car</h2>
           <label className='search-input-label' >Pick-up Location</label>
 
-          <input className='search-input' onChange={this.search} ref={el => this.searchInput =el} type='text' placeholder='city, airport, station, region, district…' />
+          <input className='search-input'
+            onFocus={this.onFocus} 
+            value={this.state.inputValue}
+            onChange={this.search}
+            ref={el => this.searchInput =el}
+            type='text' placeholder='city, airport, station, region, district…' />
           
           {
-            this.state.isLoading && <img className='search-input-loader' src='https://cdn2.rcstatic.com/images/site_graphics/newsite/preloader64.gif' alt='loading' />
+            this.state.isLoading &&
+            <img className='search-input-loader' src='https://cdn2.rcstatic.com/images/site_graphics/newsite/preloader64.gif' alt='loading' />
           }
           
-          <div className='search-results-container'>
+          <div  className={this.state.lostFocus ? 'hidden' : 'search-results'} >
           {resultsFound ? (
             
             this.state.searchResults.map((item,index)=>{
-              console.log(item);
               let iata ='';
               let city = '';
               let region = '';
               let locationType = item.bookingId.split('-')[0];
-              
+
               if (item.iata) iata = `(${item.iata})`;
               if (item.region) region = `${item.region},`;
               if (item.city) city = `${item.city},`;
-              if (locationType === 'train') locationType = 'station'
+              if (locationType === 'train') locationType = 'station';
       
               return( 
-                <div className='search-result' key={index}>
+                <div className='search-result' key={index} onClick={ () => this.selectLocation(item.name)}>
                   <div className={`search-result__type search-result__type--${locationType}`} > {locationType} </div>
                   <div className='search-result__name'>
                     {item.name} {iata}
@@ -95,9 +115,8 @@ class App extends Component {
                 </div>
               )
 
-            })
+            },this)
           ) : (
-            
             <div className='search-result search-result--disbaled'>
               No results found      
             </div>     
