631839d8fe91e0020a978fb0dbd953953df02b64
Adam <adam.a@spartadigital.co.uk>
2018-11-25 16:02:41 +0000

    created form design

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
         <div className='container'>
           <h2>Let’s find your ideal car</h2>
+          <label className='search-input-label' >Pick-up Location</label>
+          <input className='search-input' type='text' placeholder='city, airport, station, region, district…' />
+
 
         </div>
+      
       </div>
     );
   }
